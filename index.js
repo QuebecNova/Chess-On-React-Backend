b718c03745a237b10bfe8c14bb8a72c3fdcc877e
QuebecNova <selllmenya@gmail.com>
2022-07-05 00:18:16 +0300

    intial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import { instrument } from '@socket.io/admin-ui'
+import { Server } from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: true
+  }
+})
+
+const rooms = {}
+
+io.on('connection', socket => {
+  socket.emit('hi', 'connected to socket')
+  socket.on('new-game-ID', (socketID, roomID) => {
+    rooms.roomID = [socketID]
+    socket.join(roomID)
+  })
+
+  socket.on('connect-to-game', (roomID) => {
+
+    if (rooms.roomID?.length < 2) {
+      rooms.roomID.push(socket.id)
+      socket.join(roomID)
+      socket.to(roomID).emit('player-joined', 'player joined')
+    } else {
+      socket.emit('room-is-full', 'Sorry, seems this room is full!')
+      return
+    }
+  })
+
+  socket.on('move-played', (pieceOnField) => {
+    socket.broadcast.emit('piece-on-field', pieceOnField)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log('server started!');
+})
+
+instrument(io, { auth: false })
